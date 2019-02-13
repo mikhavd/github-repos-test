364fba2de04940f6255ce02c75b39fffdeae180a
Avdeev Mikhail <avdeev@micard.ru>
2019-02-13 12:36:33 +0300

    umoriliAPI removed from project

--- app/src/main/java/m13/retrofittest/main/githubUI/ItemViewHolder.java
+++ app/src/main/java/m13/retrofittest/main/githubUI/ItemViewHolder.java
@@ -6,22 +6,28 @@ import android.view.View;
 import android.widget.TextView;
 
 import m13.retrofittest.R;
+import m13.retrofittest.main.githubUI.RecyclerViewClickListener;
 
 /**
  * Created by Mikhail Avdeev on 18.12.2018.
  */
-public class ViewHolder extends RecyclerView.ViewHolder {
+public class ItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    private final RecyclerViewClickListener itemListener;
     private TextView commits;
     private TextView index;
     private TextView post;
     private TextView site;
 
-    public ViewHolder(View itemView) {
+
+    public ItemViewHolder(View itemView,
+                          RecyclerViewClickListener itemListener) {
         super(itemView);
+        this.itemListener = itemListener;
         post = (TextView) itemView.findViewById(R.id.repoitem);
         site = (TextView) itemView.findViewById(R.id.postitem_site);
         index = (TextView) itemView.findViewById(R.id.index);
         commits = (TextView) itemView.findViewById(R.id.commits);
+        itemView.setOnClickListener(this);
     }
 
     public void setPostText(String text) {
@@ -42,6 +48,12 @@ public class ViewHolder extends RecyclerView.ViewHolder {
 
     public void setCommitsText(String commits) {
         this.commits.setText(commits);
+    }
+
+    @Override
+    public void onClick(View v) {
+        itemListener.recycleViewListClicked(
+                v, this.getLayoutPosition());
     }
 }
 
